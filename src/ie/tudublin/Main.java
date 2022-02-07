248c49e398e38183a032a92b0019e8930148d5ec
C20483514 Kieran Retardo Silada <c20483514@mytudublin.ie>
2022-02-07 12:27:56 +0000

    Initial commit

---
+++ src/ie/tudublin/Main.java
@@ -0,0 +1,9 @@
+package ie.tudublin;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Every little cell in my body is happ, every little cell in my body is well");
+    }
+}
